3cda3e87ffcc95100a80d5451f7bffc043574d60
Martin Falatic <martin@falatic.com>
2015-05-05 03:00:40 -0700

    Refined

--- _examples/example.go
+++ _examples/example.go
@@ -27,15 +27,16 @@ import "os"
 import "time"
 import "math/rand"
 
+const appTitle  = "Conway's Life in Go"
+const bControl  = ".bctrl"
+const bReset    = ".breset"
+const lifeField = ".lfield"
+const lifeImage = ".limage"
+const mainchan  = "controlchannel"
+
 const init_script = `
 	grid [ttk::frame .c -padding "10 10 10 10"] -column 0 -row 0 -sticky nwes
 	grid columnconfigure . 0 -weight 1; grid rowconfigure . 0 -weight 1
-
-	ttk::button .bctrl -text "Start" -command "mainproc 0" -state disabled -width 10
-	grid .bctrl -column 0 -row 1 -padx 2 -pady 2 -sticky nwse
-
-	ttk::button .breset -text "Reset" -command "mainproc 1" -state disabled -width 10
-	grid .breset -column 0 -row 2 -padx 2 -pady 2 -sticky nwse
 `
 
 func loadPNG(filename string) image.Image {
@@ -71,18 +72,16 @@ func imageToRGBA(src image.Image) *image.RGBA {
 	return m
 }
 
-func mainproc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
-	button := ".bctrl"
-	channame := "mainproc"
+func mainproc(ir *gothic.Interpreter, animage *image.RGBA, gridx int, gridy int) {
 	recv := make(chan int, 1)
 	running := false
+	lifeMaxGens := 50
 
 	s1 := rand.NewSource(42)
     r1 := rand.New(s1)
 	r1 = r1
 
-	// register channel and enable button
-	ir.RegisterCommand(channame, func(arg int) {
+	ir.RegisterCommand(mainchan, func(arg int) {
 		select {
 			case recv <- arg:
 			default:
@@ -90,39 +89,35 @@ func mainproc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
 		}
 		fmt.Printf("Command received: running = %d\n", running)
 	})
-	ir.Eval(`%{} configure -state normal`, button)
+	ir.Eval(`%{} configure -state normal`, bControl)
 
 	for {
-		// wait for a start event
 		select {
 			case arg := <- recv:
-				println("Got something", running, arg)
+				println("Got start", running, arg)
 				break
 			default:
-				//println("Waiting", running)
 				time.Sleep(time.Second / 10)
 				continue
 		}
-		//fmt.Printf("Received %d\n", arg)
 
 		running = true
 		fmt.Printf("Starting run\n")
-		ir.Eval(`%{} configure -state normal -text "Stop"`, button)
-		ir.Eval(`%{} configure -state normal`, ".breset")
-		sizex := 800
-		sizey := 600
-		sizex /= 3
-		sizey /= 3
+		ir.Eval(`%{} configure -state normal -text "Stop"`, bControl)
+		ir.Eval(`%{} configure -state normal`, bReset)
+		scale := 3
+		sizex := gridx/scale
+		sizey := gridy/scale
 
-		animage = image.NewRGBA(image.Rect(0, 0, sizex*3, sizey*3))
-		for x := 0; x < sizex; x += 1 {
-			for y := 0; y < sizey; y += 1 {
+		animage = image.NewRGBA(image.Rect(0, 0, sizex*scale, sizey*scale))
+		for x := 0; x < sizex*scale; x += 1 {
+			for y := 0; y < sizey*scale; y += 1 {
 				animage.Set(x,y,color.RGBA{0,0,0,255})
 			}
 		}
 
 		life := gothlife.NewLife(sizex, sizey)
-		for i := 0; running && i < 1000; i++ {
+		for i := 0; running && i < lifeMaxGens; i++ {
 			life.Step()
 			for x := 0; x < sizex; x++ {
 				for y := 0; y < sizey; y++ {
@@ -131,21 +126,21 @@ func mainproc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
 					if state {
 						thiscolor = color.RGBA{0,0,255,255}
 					}
-					animage.Set(x*3+0, y*3+0, color.RGBA{0,0,0,255})
-					animage.Set(x*3+1, y*3+0, thiscolor)
-					animage.Set(x*3+2, y*3+0, color.RGBA{0,0,0,255})
-					animage.Set(x*3+0, y*3+1, thiscolor)
-					animage.Set(x*3+1, y*3+1, color.RGBA{64,0,0,255})
-					animage.Set(x*3+2, y*3+1, thiscolor)
-					animage.Set(x*3+0, y*3+2, color.RGBA{0,0,0,255})
-					animage.Set(x*3+1, y*3+2, thiscolor)
-					animage.Set(x*3+2, y*3+2, color.RGBA{0,0,0,255})
+					animage.Set(x*scale+0, y*scale+0, color.RGBA{0,0,0,255})
+					animage.Set(x*scale+1, y*scale+0, thiscolor)
+					animage.Set(x*scale+2, y*scale+0, color.RGBA{0,0,0,255})
+					animage.Set(x*scale+0, y*scale+1, thiscolor)
+					animage.Set(x*scale+1, y*scale+1, color.RGBA{64,0,0,255})
+					animage.Set(x*scale+2, y*scale+1, thiscolor)
+					animage.Set(x*scale+0, y*scale+2, color.RGBA{0,0,0,255})
+					animage.Set(x*scale+1, y*scale+2, thiscolor)
+					animage.Set(x*scale+2, y*scale+2, color.RGBA{0,0,0,255})
 				}
 			}
 			t1 := time.Now()
-			ir.UploadImage("bg", imageToRGBA(animage))
+			ir.UploadImage(lifeImage, imageToRGBA(animage))
 			fmt.Sprintf("Step %d, time %s\n", i, time.Since(t1).String())
-			ir.Eval(`%{} configure -image bg`, labelid)
+			ir.Eval(`%{} configure -image %{}`, lifeField, lifeImage)
 			time.Sleep(time.Second / 10)
 			select {
 				case arg := <- recv:
@@ -156,9 +151,9 @@ func mainproc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
 					//println("Continuing", running)
 			}
 		}
-		// reset button state
-		ir.Eval(`%{} configure -state normal -text "Start"`, button)
-		ir.Eval(`%{} configure -state disabled`, ".breset")
+
+		ir.Eval(`%{} configure -state normal -text "Start"`, bControl)
+		ir.Eval(`%{} configure -state disabled`, bReset)
 		running = false
 		fmt.Printf("Ending run\n")
 	}
@@ -167,15 +162,28 @@ func mainproc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
 func main() {
 	ir := gothic.NewInterpreter(``) // init_script can go in here too
 	ir.Eval(init_script)
-	ir.Eval(`wm title . "%{}"`, "Conway's Life in Go")
+	ir.Eval(`wm title . "%{}"`, appTitle)
+	gridx := 320
+	gridy := 240
 
-    animage := image.NewRGBA(image.Rect(0, 0, 800, 600))
-	labelid := ".l"
-	ir.UploadImage("bg", animage)
-	ir.Eval(`ttk::label %{} -image bg`, labelid)
-	ir.Eval(`grid %{} -column 0 -row 3 -sticky w`, labelid)
+	row := 0
+	col := 0
+	ir.Eval(`ttk::button %{} -text "Start" -command "%{} 0" -state disabled -width 10`, bControl, mainchan)
+	ir.Eval(`grid %{} -column %{} -row %{} -padx 2 -pady 2 -sticky nwse`, bControl, col, row)
 
-	go mainproc(ir, labelid, animage)
+	row += 1
+	col = 0
+	ir.Eval(`ttk::button %{} -text "Reset" -command "%{} 1" -state disabled -width 10`, bReset, mainchan)
+	ir.Eval(`grid %{} -column %{} -row %{} -padx 2 -pady 2 -sticky nwse`, bReset, col, row)
+
+	row += 1
+	col = 0
+    animage := image.NewRGBA(image.Rect(0, 0, gridx, gridy))
+	ir.UploadImage(lifeImage, animage)
+	ir.Eval(`ttk::label %{} -image %{}`, lifeField, lifeImage)
+	ir.Eval(`grid %{} -column %{} -row %{} -sticky w`, lifeField, col, row)
+
+	go mainproc(ir, animage, gridx, gridy)
 
 	<-ir.Done
 }
