b1b698f869b07fa48beb20661c0d9c49cb655184
Martin Falatic <martin@falatic.com>
2015-05-05 02:23:31 -0700

    Starting to work as it should

--- _examples/example.go
+++ _examples/example.go
@@ -28,14 +28,14 @@ import "time"
 import "math/rand"
 
 const init_script = `
-	wm title . "Conway's Life"
 	grid [ttk::frame .c -padding "10 10 10 10"] -column 0 -row 0 -sticky nwes
 	grid columnconfigure . 0 -weight 1; grid rowconfigure . 0 -weight 1
 
-	ttk::button .b -text "Start" -command "proc <- 0" -state disabled -width 10
-	grid .b -column 0 -row 1 -padx 2 -pady 2 -sticky nwse
+	ttk::button .bctrl -text "Start" -command "mainproc 0" -state disabled -width 10
+	grid .bctrl -column 0 -row 1 -padx 2 -pady 2 -sticky nwse
 
-	foreach w [winfo children .c] {grid configure $w -padx 5 -pady 5}
+	ttk::button .breset -text "Reset" -command "mainproc 1" -state disabled -width 10
+	grid .breset -column 0 -row 2 -padx 2 -pady 2 -sticky nwse
 `
 
 func loadPNG(filename string) image.Image {
@@ -71,20 +71,18 @@ func imageToRGBA(src image.Image) *image.RGBA {
 	return m
 }
 
-func proc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
-	button := ".b"
-	channame := "proc"
+func mainproc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
+	button := ".bctrl"
+	channame := "mainproc"
 	recv := make(chan int, 1)
+	running := false
 
-	fmt.Printf("here")
 	s1 := rand.NewSource(42)
     r1 := rand.New(s1)
 	r1 = r1
-	    
-	running := false
 
 	// register channel and enable button
-	ir.RegisterCommand(channame, func(_ string, arg int) {
+	ir.RegisterCommand(channame, func(arg int) {
 		select {
 			case recv <- arg:
 			default:
@@ -95,74 +93,89 @@ func proc(ir *gothic.Interpreter, labelid string, animage *image.RGBA) {
 	ir.Eval(`%{} configure -state normal`, button)
 
 	for {
-		// wait for an event
+		// wait for a start event
 		select {
-			case <- recv:
-				println("Got something", running)
+			case arg := <- recv:
+				println("Got something", running, arg)
 				break
 			default:
-				println("! got nothing", running)
+				//println("Waiting", running)
 				time.Sleep(time.Second / 10)
 				continue
 		}
 		//fmt.Printf("Received %d\n", arg)
 
-		if running {
-			fmt.Printf("continuing instead\n")
-			continue
-		}
 		running = true
 		fmt.Printf("Starting run\n")
-		//ir.Eval(`%{} configure -state disabled -text "Running"`, button)
 		ir.Eval(`%{} configure -state normal -text "Stop"`, button)
+		ir.Eval(`%{} configure -state normal`, ".breset")
 		sizex := 800
 		sizey := 600
-		sizex /= 2
-		sizey /= 2
+		sizex /= 3
+		sizey /= 3
 
-		animage = image.NewRGBA(image.Rect(0, 0, sizex*2, sizey*2))
+		animage = image.NewRGBA(image.Rect(0, 0, sizex*3, sizey*3))
 		for x := 0; x < sizex; x += 1 {
 			for y := 0; y < sizey; y += 1 {
 				animage.Set(x,y,color.RGBA{0,0,0,255})
 			}
 		}
 
-		l := gothlife.NewLife(sizex, sizey)
-		for i := 0; i < 10; i++ {
-			l.Step()
+		life := gothlife.NewLife(sizex, sizey)
+		for i := 0; running && i < 1000; i++ {
+			life.Step()
 			for x := 0; x < sizex; x++ {
 				for y := 0; y < sizey; y++ {
-					state := l.CurAlive(x,y)
+					state := life.CurAlive(x,y)
 					thiscolor := color.RGBA{0,0,0,255}
 					if state {
 						thiscolor = color.RGBA{0,0,255,255}
 					}
-					animage.Set(x*2+0, y*2+0, thiscolor)
-					animage.Set(x*2+1, y*2+0, thiscolor)
-					animage.Set(x*2+0, y*2+1, thiscolor)
-					animage.Set(x*2+1, y*2+1, thiscolor)
+					animage.Set(x*3+0, y*3+0, color.RGBA{0,0,0,255})
+					animage.Set(x*3+1, y*3+0, thiscolor)
+					animage.Set(x*3+2, y*3+0, color.RGBA{0,0,0,255})
+					animage.Set(x*3+0, y*3+1, thiscolor)
+					animage.Set(x*3+1, y*3+1, color.RGBA{64,0,0,255})
+					animage.Set(x*3+2, y*3+1, thiscolor)
+					animage.Set(x*3+0, y*3+2, color.RGBA{0,0,0,255})
+					animage.Set(x*3+1, y*3+2, thiscolor)
+					animage.Set(x*3+2, y*3+2, color.RGBA{0,0,0,255})
 				}
 			}
 			t1 := time.Now()
 			ir.UploadImage("bg", imageToRGBA(animage))
 			fmt.Sprintf("Step %d, time %s\n", i, time.Since(t1).String())
 			ir.Eval(`%{} configure -image bg`, labelid)
-			time.Sleep(time.Second / 120)
+			time.Sleep(time.Second / 10)
+			select {
+				case arg := <- recv:
+					println("Got an exit signal", running, arg)
+					running = false
+					break
+				default:
+					//println("Continuing", running)
+			}
 		}
 		// reset button state
 		ir.Eval(`%{} configure -state normal -text "Start"`, button)
+		ir.Eval(`%{} configure -state disabled`, ".breset")
 		running = false
 		fmt.Printf("Ending run\n")
 	}
 }
 
 func main() {
-	ir := gothic.NewInterpreter(init_script)
+	ir := gothic.NewInterpreter(``) // init_script can go in here too
+	ir.Eval(init_script)
+	ir.Eval(`wm title . "%{}"`, "Conway's Life in Go")
+
     animage := image.NewRGBA(image.Rect(0, 0, 800, 600))
 	labelid := ".l"
 	ir.UploadImage("bg", animage)
 	ir.Eval(`ttk::label %{} -image bg`, labelid)
-	ir.Eval(`grid %{} -column 0 -row 5 -sticky w`, labelid)
-	go proc(ir, labelid, animage)
+	ir.Eval(`grid %{} -column 0 -row 3 -sticky w`, labelid)
+
+	go mainproc(ir, labelid, animage)
+
 	<-ir.Done
 }
